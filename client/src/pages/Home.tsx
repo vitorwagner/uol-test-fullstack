d9b6961252055f5c93ebc8c00fee4ce8a343ce59
Vitor Wagner <vitormwagner@gmail.com>
2024-03-13 20:43:29 -0300

    Adds axios and API fetching for Home page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,12 +1,44 @@
+import { useEffect, useState } from "react";
 import Header from "../components/Header";
+import axios from "axios";
+
+interface User {
+  id: number;
+  name: string;
+  CPF: string;
+  status: string;
+}
 
 const Home = () => {
+  const API_URL = "http://localhost:8080/api/users";
+  const [users, setUsers] = useState([]);
+
+  const FetchAPI = async () => {
+    const response = await axios.get(API_URL);
+    const data = await response.data;
+    setUsers(data);
+    console.log(data);
+  }
+
+  useEffect(() => {
+    FetchAPI();
+  }, []);
+
+
   return (
     <>
     <Header />
     <div>
       <h1>Home</h1>
       <p>Welcome to the home page</p>
+    </div>
+    <div>
+      <h1>Users</h1>
+      <ul>
+        {users.map((user: User) => (
+          <li key={user.id}>{user.name} - {user.CPF} - {user.status}</li>
+        ))}
+      </ul>
     </div>
     </>
   );
