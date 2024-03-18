eede6a08a976f698c57f38cc132a5c24fd4ba99e
Vitor Wagner <vitormwagner@gmail.com>
2024-03-17 22:06:42 -0300

    Update validation messages in UserForm component

--- client/src/components/UserForm.tsx
+++ client/src/components/UserForm.tsx
@@ -9,11 +9,31 @@ interface UserFormProps {
 }
 
 const schema = Joi.object({
-  name: Joi.string().min(3).max(30).required(),
-  email: Joi.string().email({ tlds: false }).required(),
-  phone: Joi.string().length(15).required(),
-  CPF: Joi.string().length(14).required(),
-  status: Joi.string().required(),
+  name: Joi.string().min(3).max(30).required().messages({
+    'string.empty': 'Name cannot be empty',
+    'string.min': 'Name should have a minimum length of 3',
+    'string.max': 'Name should have a maximum length of 30',
+    'any.required': 'Name is a required field',
+  }),
+  email: Joi.string().email({ tlds: false }).required().messages({
+    'string.empty': 'Email cannot be empty',
+    'string.email': 'Email must be a valid email',
+    'any.required': 'Email is a required field',
+  }),
+  phone: Joi.string().pattern(new RegExp('^([1-9]{2}) (?:[2-8]|9[0-9])[0-9]{3}-[0-9]{4}$')).required().messages({
+    'string.empty': 'Phone cannot be empty',
+    'string.pattern.base': 'Phone number is not valid',
+    'any.required': 'Phone is a required field',
+  }),
+  CPF: Joi.string().length(14).required().messages({
+    'string.empty': 'CPF cannot be empty',
+    'string.length': 'CPF must be 14 characters long',
+    'any.required': 'CPF is a required field',
+  }),
+  status: Joi.string().required().messages({
+    'string.empty': 'Status cannot be empty',
+    'any.required': 'Status is a required field',
+  }),
 });
 
 const statusOptions = [
